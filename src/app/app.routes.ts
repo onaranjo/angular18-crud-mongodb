c2ebdafedc3ac803497cc6fc44b24abf3f77a6e7
Oscar Naranjo <oscarnrnj@gmail.com>
2024-10-31 15:42:21 +0100

    componentes crear, update y list de user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersCreateComponent } from './users-create/users-create.component';
+import { UsersUpdateComponent } from './users-update/users-update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: UsersListComponent
+    },
+    {
+        path: 'create', component: UsersCreateComponent
+    },
+    {
+        path: 'update', component: UsersUpdateComponent
+    }
+];
