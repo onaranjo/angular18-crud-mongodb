3d22b3c6ded42b1ad54698086a844e2acf7defce
Oscar Naranjo <oscarnrnj@gmail.com>
2024-10-31 10:45:49 +0100

    adicion del bootstrap y crear  el router-outlet

--- backend/configs/dbConnection.js
+++ backend/configs/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connetDB = mongoose.connect(process.env.CONN_STR + '/test')
-  .then(() => console.log('Connected!'))
+  .then(() => console.log('Connected!!!'))
   .catch((e)=>{
     console.log('Database no conected', e)
   });
